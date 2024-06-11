a1dc744cc530a08cd31a553d6635426fdea2c448
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2024-06-11 09:46:18 -0300

    Ponderada 3 - Detalhes

--- Ponderada 3/src/service-user/main.py
+++ Ponderada 3/src/service-user/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.routes import user_routes
-from app.database import engine
-from app.models import user_model
+from routes import user_routes
+from database import engine
+from models import user_model
 
 app = FastAPI()
 
